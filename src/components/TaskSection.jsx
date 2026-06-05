df42ce502f976281afa20e72bd5ad4f098e40cd7
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-06-05 20:10:53 +0530

    feat: refactor TaskSection to dynamically render task columns

--- src/components/TaskSection.jsx
+++ src/components/TaskSection.jsx
@@ -1,31 +1,41 @@
 import TaskCard from "./TaskCard";
 
-const TaskSection = () => {
+const TaskSection = ({ taskObject = [] }) => {
+  const columns = [
+    { title: "📋 Planning", statusCheck: "Planning" },
+    { title: "🚧 In Progress", statusCheck: "In Progress" },
+    { title: "✅ Done", statusCheck: "Done" },
+  ];
   return (
-    <section className="w-23/24 mx-auto flex-1 p-4 mb-2 border-(--border) border theme-transition rounded-xl  bg-(--surface)   flex gap-4 overflow-y-auto justify-center items-start scrollbar-hide ">
-      {/* section-1 */}
-      <div className="   flex-1   flex flex-col  gap-5 ">
-        {/* title */}
-        <div className="p-4 border-b border-(--border) ">
-          <p className="font-medium  text-center ">📋 Planning</p>
-        </div>
-      </div>
-
-      {/* section-2*/}
-      <div className="   flex-1   flex flex-col  gap-5">
-        {/* title */}
-        <div className="p-4 border-b border-(--border)">
-          <p className="font-medium  text-center ">🚧 In Progress</p>
-        </div>
-      </div>
-
-      {/* section-3*/}
-      <div className="   flex-1   flex flex-col  gap-5">
-        {/* title */}
-        <div className="p-4 border-b border-(--border)">
-          <p className="font-medium  text-center ">✅ Done</p>
-        </div>
-      </div>
+    <section className="w-[96%]  min-h-0   mx-auto flex-1 p-5 mb-2 border-(--border) border rounded-xl  bg-(--surface)  md:flex-row flex flex-col gap-5 overflow-y-auto justify-start items-center md:justify-center md:items-start scrollbar-hide  ">
+      {columns.map(({ title, statusCheck }) => {
+        return (
+          <div
+            key={statusCheck}
+            className="   md:w-[30%] w-[95%] flex flex-col  gap-5 "
+          >
+            <div className="p-4 border-b border-(--border) ">
+              <p className="font-medium  text-center ">{title}</p>
+            </div>
+            {taskObject
+              ?.filter(({ status }) => {
+                return status === statusCheck;
+              })
+              .map(({ id, title, description, status, priority }) => {
+                return (
+                  <TaskCard
+                    key={id}
+                    title={title}
+                    description={description}
+                    status={status}
+                    priority={priority}
+                    id={id}
+                  />
+                );
+              })}
+          </div>
+        );
+      })}
     </section>
   );
 };
